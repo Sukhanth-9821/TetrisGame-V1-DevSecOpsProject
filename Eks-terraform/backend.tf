fc6a37ea49a0f3ddb7b2bcc9b203b15ae8cce519
Sukhanth-9821 <83202428+Sukhanth-9821@users.noreply.github.com>
2024-01-09 21:22:46 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "awsdevsecopsprojtetris" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Jenkins/backend/terraform.tfstate"
     region = "ap-south-1"
   }
 }
